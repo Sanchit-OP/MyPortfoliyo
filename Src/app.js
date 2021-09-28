2943b21d80d7c4761013752dd42243ff60ee1046
Sanchit-OP <sanchitahlawat2003@gmail.com>
2021-09-28 16:38:05 +0530

    Commit 2
    Added animation to the the home page text

--- Src/app.js
+++ Src/app.js
@@ -1,10 +1,14 @@
 document.addEventListener("scroll", () => {
     if (window.scrollY > 717) {
         var parr = document.getElementById("abb");
+
         parr.classList.add("active");
+
     }
     if (window.scrollY < 717) {
         var parr = document.getElementById("abb");
+
         parr.classList.remove("active");
+
     }
 });
